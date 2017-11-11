4dc498ec0013cb39e08f8d65631e9c0791d16c6d
Paul Hooijenga <paulhooijenga@gmail.com>
2017-11-11 03:45:47 +0100

    Save answer after selecting

--- js/functions.js
+++ js/functions.js
@@ -6,31 +6,44 @@
 $(".input").each(function(){
 	var answers = $(this).find(".answer");
 	var questionId = $(this).data('number');
-	
-	
+
+
 	//set click event
 	answers.each(function(){
 		$(this).click(function () {
 			var fieldVal = $(this).data('value');
-			
-			//update hidden field
-			$(".hiddenForm").find("input[name='"+questionId+"']").val(fieldVal);
-			
+
+			saveAnswer(questionId, fieldVal);
+
 			//updateClass
 			answers.each(function(){
 				$(this).removeClass("active");
 			});
 			$(this).addClass("active");
-			
+
 			//set Hash
 			setTimeout(function(){
 				window.location.hash = "#question"+(questionId+1);
 			}, 250);
-			
+
 		});
 	});
 });
 
+var session;
+
+function saveAnswer(questionId, fieldVal) {
+    var params = {}, data = {};
+    if (session) {
+        params.session = session;
+    }
+
+    data[questionId] = fieldVal;
+
+    jQuery.post("http://cerberus.hackedd.nl/vie2017/?" + jQuery.param(params), data, function (response) {
+        session = response.session;
+    });
+}
 
 $(window).on('hashchange',function(){
 	onloadHashFunction();
@@ -43,18 +56,21 @@ $(document).ready(function(){
 
 function onloadHashFunction() {
 	var currentHash = location.hash.slice(1);
+
+	if (!currentHash) return;
+
 	var currentId = currentHash.match(/\d+/); // 123456
-	
-	
+
+
 	//remove current input  scale trans off by remove class active. + timeout 150ms for display:none;
-	var elem = $(".input.active")
+	var elem = $(".input.active");
 	if($(elem)) {
 		$(elem).removeClass('active');
 		setTimeout(function(){
 			$(elem).css("display", "none");
 		}, 150);
 	}
-	
+
 	//set current input display:block; + timeout for scale trans. add class active
 	setTimeout(function(){
 		$(".input[data-number="+currentId+"]").css("display" , "block");
@@ -62,12 +78,11 @@ function onloadHashFunction() {
 			$(".input[data-number="+currentId+"]").addClass('active');
 		}, 10);
 	}, 160);
-	
+
 	//set question
 	var questionText = $(".input[data-number="+currentId+"]").data('question');
 	$(".question h1").text("Vraag "+currentId+": "+questionText);
-	
-	//set previous and next btns
-	
-};
 
+	//set previous and next btns
+
+}
