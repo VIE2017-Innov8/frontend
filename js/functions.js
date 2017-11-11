58968d7b783def0215ebb1d4abcba470a7a33f79
Jorrik Klijnsma <jklijnema@hotmail.nl>
2017-11-11 01:58:12 +0100

    First commit

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,22 @@
 /**
  * Created by jorrikklijnsma on 11/11/2017.
  */
+
+
+$(".input").each(function(){
+	var answers = $(this).find(".answer");
+	var questionId = $(this).data('number');
+	var questionText = $(this).data('question');
+	
+	
+	//set click event
+	answers.each(function(){
+		$(this).click(function () {
+			console.log($(".hiddenForm").find("input[name='"+questionId+"']"))
+			var fieldVal = $(this).data('value');
+			
+			//update hidden field
+			$(".hiddenForm").find("input[name='"+questionId+"']").val(fieldVal);
+		});
+	});
+});
